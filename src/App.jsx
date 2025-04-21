b138b85045c9ad20bd97fe6dcd5328fd00615735
Jan-Frederik de Graaf <jan-frederik.de.graaf@student.howest.be>
2025-04-21 17:20:57 -0400

    react addition

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <img src="./leap.svg" className="logo leap" alt="Leap logo" />
+
+      </div></>)
+}
+
+export default App
